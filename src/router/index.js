0e749dca78f4cc5757ebef04e710e438f9942a54
Marcelo <santosmarcelo1995@live.com>
2021-04-25 11:42:19 -0300

    Add router

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Home = () => import('../views/Home/index.vue')
+const History = () => import('../views/History/index.vue')
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/history',
+    name: 'History',
+    component: History,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: 'Home' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
