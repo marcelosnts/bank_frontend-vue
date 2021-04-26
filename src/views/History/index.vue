1b4738f43bb6942633a782841862517a9d3cef0d
Marcelo <santosmarcelo1995@live.com>
2021-04-25 23:52:27 -0300

    Add history and get values from api

--- src/views/History/index.vue
+++ src/views/History/index.vue
@@ -1,7 +1,33 @@
 <template>
     <h1>History</h1>
+    {{state}}
 </template>
 
 <script>
-    export default {}
+    import { reactive, onMounted } from 'vue'
+    import services from '../../services'
+
+    export default {
+        setup () {
+            const state = reactive({
+                balance: 0,
+                deposits: 0,
+                withdraws: 0,
+                transactions: {}
+            })
+
+            onMounted(async () => {
+                const { data } = await services.transactions.history()
+                
+                state.balance = data.balance
+                state.deposits = data.deposits
+                state.withdraws = data.withdraws 
+                state.transactions = data.transactions
+            })
+
+            return {
+                state
+            }
+        }
+    }
 </script>
